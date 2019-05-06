a397f0bbf55131315f34641d9d1490d84c4d3031
WKNITTING <ythanakron@gmail.com>
2019-05-06 14:57:22 +0700

    Add env and connect to mlab

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  // Use Routes
 app.use('/api/inventory', inventory)
 
 const port = process.env.PORT || 5000
